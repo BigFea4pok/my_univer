6c93e1f5a6ee95ef7e52a35f84f348f0f29409bb
Fea4pok <bigfea4pok@gmail.com>
2025-11-26 01:26:39 +0200

    26.11.25. Пилю конкатенацию

--- lab3/lab3.1.1.cpp
+++ lab3/lab3.1.1.cpp
@@ -1,35 +1,53 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
-char* enter_str(int len){
-    if(len <= 0){
+char* get_str(int len){
+    if (len <= 0){
         char *str = new char[1];
         str[0] = '\0';
         return str;
     }
 
-    char *str = new char[len + 1];
-    cout << "Enter your string: ";
-    cin >> str;         // если строка меньше длины cin ставит \0 последним элементом
-    str[len] = '\0';    // есть риск переполнения, но строка хотя бы всегда на \0 заканчивается
-    cout << "Your string is - \"" << str <<"\"\n\n";
+    char* str = new char[len + 1];
+
+    // очистка от прошлого ввода
+    int ch = getchar();
+    if (ch != '\n' && ch != EOF){
+        ungetc(ch, stdin);
+    }
+
+    if (fgets(str, len + 1, stdin) != NULL) {
+        int i = 0;
+        int truncated = 0;
+
+        /* Идём по буферу, пока не встретим '\0' или '\n' */
+        while (str[i] != '\0' && str[i] != '\n') {
+            i++;
+        }
+
+        if (str[i] == '\n') {
+            /* Заменяем финальный перевод строки на '\0' */
+        str[i] = '\0';
+        } else {
+            /* Нет '\n' — возможно ввод был длиннее буфера.
+               Очистим остаток в stdin до конца строки. */
+            int ch;
+            while ((ch = getchar()) != '\n' && ch != EOF) { /* пусто */ }
+            truncated = 1;
+        }
+
+        printf("Результат: \"%s\"%s\n", str, truncated ? " (ввод был длиннее буфера, обрезано)" : "");
+    }
 
     return str;
-}
-
-int get_real_len(const char* str){
-    int n = 0;
-    while(str[n] != '\0') ++n;
-    return n;
 }
 
 char* concatenate(const char *str1, int len1, const char *str2, int len2){
     
     char *conc_str = new char[len1 + len2 + 1];
-    for(int i = 0; i < (len1); ++i){
+    for (int i = 0; i < (len1); ++i){
         conc_str[i] = str1[i];
     }
-    for(int i = len1, j = 0; i < (len1 + len2); ++i, ++j){
+    for (int i = len1, j = 0; i < (len1 + len2); ++i, ++j){
         conc_str[i] = str2[j];
     }
     conc_str[len1 + len2] = '\0';
@@ -38,20 +56,22 @@ char* concatenate(const char *str1, int len1, const char *str2, int len2){
 }
 
 int main() {
-    cout << "Enter length of string №1: ";
-    int len1; cin >> len1;
-    char *str1 = enter_str(len1);
+    printf("Введите длину строку №1: ");
+    int len1; scanf("%d", &len1);
+    char *str1 = get_str(len1);
+    printf("%s", str1);
 
-    cout << "Enter length of string №2: ";
-    int len2; cin >> len2;
-    char *str2 = enter_str(len2);
+    printf("Введите длину строку №2: ");
+    int len2; scanf("%d", &len2);
+    char *str2 = get_str(len2);
+    printf("%s", str2);
 
     // На случай, если строка оказалась меньше заданной длины
-    len1 = get_real_len(str1);
-    len2 = get_real_len(str2);
+    // len1 = get_real_len(str1);
+    // len2 = get_real_len(str2);
 
     char *conc = concatenate(str1, len1, str2, len2);
-    cout << "Concatenated - \"" << conc << "\"\n";
+    printf("Concatenated - \"%s\"\n", conc);
 
     delete[] str1;
     delete[] str2;
